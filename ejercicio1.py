52e06c63f3d3a97bcf7e4b672729b137ad2f771e
Diegopj22 <dpj2202@gmail.com>
2022-12-08 11:44:12 +0100

    Update ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+# desarrollar un reloj
+import datetime 
+import time
+import os
+
+while True:
+    os.system("clear")
+    dt=datetime.datetime.now()
+    print("{}:{}:{}".format(dt.hour,dt.minute,dt.second))
+    
